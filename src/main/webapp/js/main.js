8fc9f467a58bcb1e4a37c160bf84d5a1319aa246
Peter <info@jetsli.de>
2012-11-12 20:19:24 +0100

    not using localhost

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -17,10 +17,10 @@ var toCoord = {
 };
 var bounds = {};
 // local development
-var host = "http://localhost:8989";
+//var host = "http://localhost:8989";
 
 // cross origin:
-//var host = "http://217.92.216.224:8080";
+var host = "http://217.92.216.224:8080";
 
 $(document).ready(function(e) {
     // I'm really angry about you history.js :/ (triggering double events) ... but let us just use the url rewriting thing
