e612a5bbfea06b38fe6ff13a008fb99983448714
Peter <peathal@yahoo.de>
2013-02-10 20:50:03 +0100

    reduce memory requirements

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-export MAVEN_OPTS="$MAVEN_OPTS -Xms2600m -Xms2600m -XX:PermSize=40m -XX:MaxPermSize=40m"; mvn jetty:run
+export MAVEN_OPTS="$MAVEN_OPTS -Xms1600m -Xms1600m -XX:PermSize=40m -XX:MaxPermSize=40m"; mvn jetty:run
